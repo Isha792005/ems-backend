411fdf84af5190af2f43d70f94130a2369b782a4
Isha792005 <isha2005bhoite@gmail.com>
2026-07-23 11:02:21 +0530

    cors updated

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ const rateLimiter = rateLimit({
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://your-frontend-url.vercel.app",
+    credentials: true,
+  }),
+);
 app.get("/", (req, res) => {
   res.json("msg:Ritika");
 });
